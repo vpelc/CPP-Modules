3d17230d7e5435569abc838b4a5cc99bc016737e
Victor Pelc <vpelc@fu-r5-p1.s19.be>
2025-01-07 13:06:17 +0100

    q_q

--- CPP-00/ex02/Account.cpp
+++ CPP-00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: vpelc <vpelc@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 15:46:33 by vpelc             #+#    #+#             */
-/*   Updated: 2025/01/07 12:31:03 by vpelc            ###   ########.fr       */
+/*   Updated: 2025/01/07 13:05:55 by vpelc            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,16 @@ int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
 
+/*
+	Constructor : creates the object (account)
+	And 	.displays timestamp
+			.initializes the index of the account 
+				and increases total of accounts
+			.initializes the amount of the account 
+				and adds it to the total amount
+			.initializes number of deposits and withdrawals to 0
+			.displays : index | amount and that it's created
+*/
 Account::Account( int initial_deposit )
 {
 	_displayTimestamp();
@@ -33,6 +43,11 @@ Account::Account( int initial_deposit )
 	std::cout << ";created" << std::endl;
 }
 
+/*
+	Destructor : destroys the objects (account)
+	And 	.displays timestamp
+			.displays : index | amount and that it's closed
+*/
 Account::~Account( void )
 {
 	_displayTimestamp();
@@ -40,6 +55,9 @@ Account::~Account( void )
 	std::cout << ";closed" << std::endl;
 }
 
+/*
+	Displays the timestamp in the format [YYYYMMDD_hhmmss]
+*/
 void	Account::_displayTimestamp( void )
 {
 	std::time_t result = std::time(NULL);
@@ -68,6 +86,13 @@ int	Account::getNbWithdrawals( void )
 {
 	return _totalNbWithdrawals;
 }
+
+/*
+
+	Displays : 	.timestamp
+				.number of accounts | total of the amounts 
+				| total of deposits | total of withdrawals
+*/
 void	Account::displayAccountsInfos( void )
 {
 	_displayTimestamp();
@@ -77,25 +102,46 @@ void	Account::displayAccountsInfos( void )
 	
 }
 
+/*
+	.displays timestamp
+	.increases the amount of the account by the arg: deposit
+		and the total amount
+	.increases the amount of the number of deposits 
+		and the total of deposits
+	.displays : index | old amount | deposit 
+		| new amount | number of deposits
+*/
 void	Account::makeDeposit( int deposit )
 {
-	_displayTimestamp();
+	int	p_amount = _amount;
+
 	/*if (deposit < 0)	//--> Added security to prevent 
 		return;			//negative amounts on the deposits	*/
-	std::cout << "index:" << _accountIndex << ";p_amount:" << _amount;
-	std::cout << ";deposit:" << deposit;
+	_displayTimestamp();
 	_amount += deposit;
 	_totalAmount += deposit;
 	_nbDeposits++;
 	_totalNbDeposits++;
+	std::cout << "index:" << _accountIndex << ";p_amount:" << p_amount;
+	std::cout << ";deposit:" << deposit;
 	std::cout << ";amount:" << _amount;
 	std::cout << ";nb_deposits:" << _nbDeposits << std::endl;
 }
+
+/*
+	.displays timestamp
+	.decreases the amount of the account by the arg: withdrawal
+		and the total amount
+	.increases the amount of the number of withdrawals 
+		and the total of withdrawals
+	.displays : index | old amount | deposit 
+		| new amount | number of withdrawals
+*/
 bool	Account::makeWithdrawal( int withdrawal )
 {
-	_displayTimestamp();
 	/*if (withdrawal < 0)	//--> Added security to prevent 
 		return;				//negative amounts on the withdrawal	*/
+	_displayTimestamp();
 	std::cout << "index:" << _accountIndex << ";p_amount:" << _amount;
 	std::cout << ";withdrawal:";
 	if (withdrawal > _amount)
@@ -117,6 +163,12 @@ int		Account::checkAmount( void ) const
 {
 	return _amount;
 }
+
+/*
+	.displays timestamp
+	.displays : index | amount | 
+		number of deposits | number of withdrawals
+*/
 void	Account::displayStatus( void ) const
 {
 	_displayTimestamp();
