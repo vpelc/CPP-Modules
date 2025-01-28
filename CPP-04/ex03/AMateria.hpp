570a563ac1391dd48dbcf936deefceb166c4952f
Victor Pelc <vpelc@shi-r4-p6.s19.be>
2025-01-28 17:07:01 +0100

    ;_;$

--- CPP-04/ex03/AMateria.hpp
+++ CPP-04/ex03/AMateria.hpp
@@ -6,21 +6,27 @@
 /*   By: vpelc <vpelc@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 15:28:37 by vpelc             #+#    #+#             */
-/*   Updated: 2025/01/28 15:29:35 by vpelc            ###   ########.fr       */
+/*   Updated: 2025/01/28 17:04:32 by vpelc            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef AMATERIA_HPP
 #define AMATERIA_HPP
 
+# include <string>
+# include <iostream>
+
 class AMateria
 {
-	protected:
-		[...]
-		public:
-		AMateria(std::string const & type);
-		[...]
-		std::string const & getType() const; //Returns the materia type
-		virtual AMateria* clone() const = 0;
-		virtual void use(ICharacter& target);
+	protected : 
+	
+	
+	public :
+						AMateria(std::string const & type);
+		
+	std::string const 	&getType() const; //Returns the materia type
+	virtual AMateria*	clone( void ) const = 0;
+	virtual void 		use(ICharacter& target);
 };
+
+#endif
