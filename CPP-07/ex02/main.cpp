d54d5eb17a17545188a6dddfcf21beaaa462d949
Victor <pelcvictor@gmail.com>
2025-08-19 15:02:23 +0200

    cpp07 last exercise

--- CPP-07/ex02/main.cpp
+++ CPP-07/ex02/main.cpp
@@ -1,8 +1,18 @@
 #include "utils/Array.hpp"
+#include <iostream>
 
 int main()
 {
 	
-
+    Array<int> new_array(10);
+    new_array[0] = 14;
+    new_array[1] = 12;
+	for (int i = 0; i < new_array.size(); i++)
+	{
+		if (i + 1 != new_array.size())
+    		std::cout << new_array[i] << " , ";
+		else
+    		std::cout << new_array[i] << std::endl;
+	}
 	return 0;
 }
--- CPP-07/ex02/utils/Array.hpp
+++ CPP-07/ex02/utils/Array.hpp
@@ -1,17 +1,95 @@
 #pragma once
 
+#include <exception>
+
 template <typename Type>
 class Array {
 
 	private :
-		Type 		*T;
+		Type 			*T;
+		int				_size;
 
 	public :
-					Array();
-					Array(unsigned int n);
-					Array(const Array& copy);
-		Array		&operator=(Array &other);
-		T			&operator[](int index);
-		int			size();
-		
+						Array();
+						Array(unsigned int n);
+						Array(const Array<Type>& copy);
+						~Array();
+		Array<Type>		&operator=(Array<Type> &other);
+		Type			&operator[](int index);
+		int				size() const;
+
+		class InvalidIndexException : public std::exception
+		{
+			public :
+				const char* what() const throw();	
+		};
 };
+
+
+template <typename Type>
+Array<Type>::Array()
+{
+	T = new Type[0];
+    _size = 0;
+}
+
+
+template <typename Type>
+Array<Type>::Array(unsigned int s)
+{
+    T = new Type[s];
+	_size = s;
+    for (unsigned int i = 0; i < s; i++)
+        T[i] = 0;
+}
+
+
+template <typename Type>
+Array<Type>::Array(Array const &src) : T(src.T)
+{
+    *this = src;
+}
+
+
+template <typename Type>
+Array<Type>::~Array()
+{
+    delete[] T;
+}
+
+
+template <typename Type>
+int Array<Type>::size() const
+{
+    return _size;
+}
+
+
+template <typename Type>
+Array<Type> &Array<Type>::operator=(Array<Type> &other)
+{
+    if (this != &other)
+    {
+        delete[] this->T;
+        this->T = new Type[other.size()];
+        for (int i = 0; i < other.size(); i++)
+            this->T[i] = other.T[i];
+    }
+    return (*this);
+}
+
+
+template <typename Type>
+Type &Array<Type>::operator[](int index)
+{
+    if (index >= _size || index < 0)
+        throw Array::InvalidIndexException();
+    return (this->T[index]);
+}
+
+
+template <typename Type>
+const char *Array<Type>::InvalidIndexException::what() const throw()
+{
+    return ("Index not valid");
+}
